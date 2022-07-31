d0d11047023ea68df216fba01c758b3557823fee
molledom <miguel.molledo.alvarez@gmail.com>
2022-07-31 21:50:41 +0200

    Finished token transactions with test

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -15,16 +15,41 @@ contract DappToken {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
     // Track balanaces
     mapping(address => uint256) public balanceOf;
     // Send Tokens
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
+    constructor(
+    	string memory _name,
+    	string memory _symbol,
+    	uint256 _totalSupply
+    	){
     	name = _name;
     	symbol = _symbol;
     	totalSupply = _totalSupply * (10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 
     }
+
+    function transfer(address _to, uint256 _value)
+	    public 
+	    returns (bool success)
+	    {
+
+	    	// if (balanceOf[msg.sender] > _value)){
+
+	    	// }
+	    	require( balanceOf[msg.sender] >= _value);
+	    	require(_to != address(0));
+
+	    	balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+	    	balanceOf[_to] =  balanceOf[_to] + _value;
+	    	
+	    	// emit Event
+	    	emit Transfer(msg.sender, _to, _value);
+	    	return true;
+
+	    }
 
 }
--- test/Token.js
+++ test/Token.js
@@ -3,24 +3,30 @@ const {expect } = require('chai')
 
 // const hardhat = require('hardhat')
 
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+
 
 
 
 describe('Token', ()=>{
-
+	const name = 'Dapp University'
+	const symbol = 'DAPP'
+	const decimals = '18'
+	const totalSupply = '1000000'
 	async function deployedToken(){
 		const Token = await ethers.getContractFactory('DappToken')
 		token = await Token.deploy('Dapp University', 'DAPP', 1000000)
 		const accounts = await ethers.getSigners()
-		const deployer = accounts[0]
-		return {token, deployer}
+		const deployer = accounts.shift()
+		// const deployer = accounts[0]
+		return {token, deployer, accounts}
 	}
 
 	describe('Deployment', ()=>{
-		const name = 'Dapp University'
-		const symbol = 'DAPP'
-		const decimals = '18'
-		const totalSupply = '1000000'
+
 
 		it('Has correct name', async () => {
 			const {token} = await waffle.loadFixture(deployedToken)
@@ -46,6 +52,80 @@ describe('Token', ()=>{
 
 	})
 
+
+
+	describe('Sending Tokens', () => {
+
+		let ammount, transaction, result, token, accounts, deployer
+		describe('success Cases', ()=>{
+
+			
+			beforeEach( async ()=>{
+				Token = await ethers.getContractFactory('DappToken')
+				token = await Token.deploy(name, symbol, 1000000)
+				accounts = await ethers.getSigners()
+				deployer = accounts.shift()
+
+				ammount = tokens(100)
+				transaction = await token.connect(deployer).transfer(accounts[0].address, ammount)
+				result = await transaction.wait()
+
+
+			})
+
+
+			it ('Sending Token', async () => {
+				expect(await token.balanceOf(accounts[0].address)).to.equal(ammount)
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+				
+
+			})
+
+			it ('Emits a Transfer event', async ()=>{
+
+				// Checking Event Transfer
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				// Checking Arguments
+				expect(event.args['to']).to.equal(accounts[0].address)
+				expect(event.args['from']).to.equal(deployer.address)
+				expect(event.args['value']).to.equal(ammount)
+
+			})
+
+		})
+
+		describe('Failure Cases', () => {
+
+			it('rejects insufficient balances', async ()=>{
+				//transfer more tokens than deployer has.
+				const invalidAmount = tokens(totalSupply + 1)
+				await expect(token.connect(deployer).transfer(accounts[0].address, invalidAmount)).to.be.reverted
+
+			})
+
+			it('rejects invalid recipient', async ()=>{
+				const amount = tokens(100)
+				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+
+
+			})
+
+
+		})
+
+
+	})
+
+
+
+
+})
+
+
+
+
 	//  WITH HOOK Before each
 	// let token
 	// beforeEach(async ()=>{
@@ -55,8 +135,3 @@ describe('Token', ()=>{
 	// })
 
 	// all tests
-
-
-})
-
-
