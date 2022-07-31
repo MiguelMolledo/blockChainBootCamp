b0db94f234b9146e8acddac0907f157a61de3273
molledom <miguel.molledo.alvarez@gmail.com>
2022-07-31 19:54:47 +0200

    tokenBalances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -15,10 +15,16 @@ contract DappToken {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    // Track balanaces
+    mapping(address => uint256) public balanceOf;
+    // Send Tokens
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply){
     	name = _name;
     	symbol = _symbol;
     	totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
+
     }
 
 }
--- test/Token.js
+++ test/Token.js
@@ -11,14 +11,16 @@ describe('Token', ()=>{
 	async function deployedToken(){
 		const Token = await ethers.getContractFactory('DappToken')
 		token = await Token.deploy('Dapp University', 'DAPP', 1000000)
-		return {token}
+		const accounts = await ethers.getSigners()
+		const deployer = accounts[0]
+		return {token, deployer}
 	}
 
 	describe('Deployment', ()=>{
 		const name = 'Dapp University'
 		const symbol = 'DAPP'
 		const decimals = '18'
-		const supply = '1000000'
+		const totalSupply = '1000000'
 
 		it('Has correct name', async () => {
 			const {token} = await waffle.loadFixture(deployedToken)
@@ -34,9 +36,14 @@ describe('Token', ()=>{
 		})
 		it('Has correct Total Supply', async () => {
 			const {token} = await waffle.loadFixture(deployedToken)
-			const value = ethers.utils.parseUnits(supply, 'ether')
+			const value = ethers.utils.parseUnits(totalSupply, 'ether')
 			expect(await token.totalSupply()).to.equal(value)
 		})		
+		it('Assigns Total Supply to deployer', async () => {
+			const {token, deployer} = await waffle.loadFixture(deployedToken)
+			expect(await token.totalSupply()).to.equal(await token.balanceOf(deployer.address))
+		})		
+
 	})
 
 	//  WITH HOOK Before each
