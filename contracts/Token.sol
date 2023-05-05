fe39ab95b2e7cc0d829b0f132263f3d343a72668
mmolledo <miguel.molledo.alvarez@gmail.com>
2023-05-05 18:43:16 +0200

    stable and added testing for allowance and transfer methods

---
+++ contracts/Token.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// Import this file to use console.log
+import 'hardhat/console.sol';
+
+// console.log('Unlock time is %o and block timestamp is %o', unlockTime, block.timestamp);
+contract Token {
+    // string public name = 'Dapp University';
+    // string public symbol = 'DAPP';
+    
+    // uint256 public totalSupply = 1000000 * (10**decimals);
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+	event Approval(address indexed owner,
+	 address indexed spender,
+	 uint256 value
+	 );
+    // Track balanaces
+    mapping(address => uint256) public balanceOf;
+    // Send Tokens
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(
+    	string memory _name,
+    	string memory _symbol,
+    	uint256 _totalSupply
+    	){
+    	name = _name;
+    	symbol = _symbol;
+    	totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
+
+    }
+
+
+
+    function transfer(address _to, uint256 _value)
+	    public 
+	    returns (bool success)
+	    {
+
+	    	// if (balanceOf[msg.sender] > _value)){
+
+	    	// }
+	    	require( balanceOf[msg.sender] >= _value);
+	    	require(_to != address(0));
+
+	    	balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+	    	balanceOf[_to] =  balanceOf[_to] + _value;
+	    	
+	    	// emit Event
+	    	emit Transfer(msg.sender, _to, _value);
+	    	return true;
+
+	    }
+
+
+	function approve(address _spender, uint256 _value)
+		public returns (bool success)
+		{
+			require(_spender != address(0));
+
+			// allowance[msg.sender][_spender.address] = 0;
+			allowance[msg.sender][_spender] = _value;
+			require(allowance[msg.sender][_spender] == _value);
+	    	emit Approval(msg.sender, _spender, _value);
+
+			return true;
+
+		}
+
+}
---
+++ test/Token.js
@@ -0,0 +1,169 @@
+const { ethers, waffle} = require('hardhat')
+const {expect } = require('chai')
+
+// const hardhat = require('hardhat')
+
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+
+
+
+
+describe('Token', ()=>{
+
+	let token, accounts, deployer, receiver, exchange
+
+	beforeEach( async ()=>{
+		const Token = await ethers.getContractFactory('Token')
+		token = await Token.deploy('Dapp University', 'DAPP', 1000000)
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]
+		receiver = accounts[1]
+		exchange = accounts[2]
+
+	})
+
+	describe('Deployment', ()=>{
+		const name = 'Dapp University'
+		const symbol = 'DAPP'
+		const decimals = '18'
+		const totalSupply = '1000000'
+
+		it('Has correct name', async () => {
+			expect(await token.name()).to.equal(name)
+		})
+		it('Has correct Symbol', async () => {
+			expect(await token.symbol()).to.equal(symbol)
+		})
+			it('Has correct Decimals', async () => {
+			expect(await token.decimals()).to.equal(decimals)
+		})
+		it('Has correct Total Supply', async () => {
+			const value = ethers.utils.parseUnits(totalSupply, 'ether')
+			expect(await token.totalSupply()).to.equal(value)
+		})		
+		it('Assigns Total Supply to deployer', async () => {
+			// const {token, deployer} = await waffle.loadFixture(deployedToken)
+			expect(await token.totalSupply()).to.equal(await token.balanceOf(deployer.address))
+		})		
+
+	})
+
+
+
+	describe('Sending Tokens', () => {
+
+		let ammount, transaction, result
+		describe('success Cases', ()=>{
+
+			
+			beforeEach( async ()=>{
+				ammount = tokens(100)
+				transaction = await token.connect(deployer).transfer(receiver.address, ammount)
+				result = await transaction.wait()
+
+
+			})
+
+
+			it ('Transfer Token Balances', async () => {
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+				expect(await token.balanceOf(receiver.address)).to.equal(ammount)
+				
+
+			})
+
+			it ('Emits a Transfer event', async ()=>{
+
+				// Checking Event Transfer
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				// Checking Arguments
+				const args = event.args
+				expect(event.args.to).to.equal(receiver.address)
+				expect(event.args.from).to.equal(deployer.address)
+				expect(event.args.value).to.equal(ammount)
+
+			})
+
+		})
+
+		describe('Failure Cases', () => {
+			const totalSupply = tokens(1000000)
+			it('rejects insufficient balances', async ()=>{
+				//transfer more tokens than deployer has.
+				const invalidAmount = tokens(totalSupply + 1)
+				await expect(token.connect(deployer).transfer(accounts[0].address, invalidAmount)).to.be.reverted
+
+			})
+
+			it('rejects invalid recipient', async ()=>{
+				const amount = tokens(100)
+				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+
+
+			})
+
+		})
+
+
+	})
+
+
+	describe('Approving Tokens', () => {
+
+		let amount, transaction, result
+
+		beforeEach(async ()=>{
+			amount = tokens(100)
+			transaction = await token.connect(deployer).approve(exchange.address, amount)
+			result = await transaction.wait()
+		})
+
+		describe('Success', () => {
+
+			it('allocates an allowance for delegated Token Spending', async ()=>{
+				expect(amount).to.equal(await token.allowance(deployer.address, exchange.address))
+			})
+
+			it('Emmits an approval Event', async ()=>{
+
+				const event = result.events[0]
+				expect(event.event).to.equal('Approval')
+
+				// Checking Arguments
+				const args = event.args
+				expect(event.args.spender).to.equal(exchange.address)
+				expect(event.args.value).to.equal(amount)
+			})
+
+
+		})
+
+	    describe('Failure', () => {
+	      it('rejects invalid spenders', async () => {
+	        await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+	      })
+	    })
+
+	})
+
+
+
+})
+
+
+
+
+	//  WITH HOOK Before each
+	// let token
+	// beforeEach(async ()=>{
+	// 	const Token =a wait ethers.getContractFactory('Token')
+	// 	token = await Token.deploy()
+	// 	return token
+	// })
+
+	// all tests
